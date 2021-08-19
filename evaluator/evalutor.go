e4002df95af5141882683c3a398116951a120a58
kabironline <kabir.maniar@maniartech.com>
2021-08-19 16:27:20 +0530

    Added Boolean and Null Evalution Capablity

--- evaluator/evalutor.go
+++ evaluator/evalutor.go
@@ -5,6 +5,12 @@ import (
 	"github.com/kabironline/monke/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
@@ -15,6 +21,9 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+
 	}
 	return nil
 }
@@ -24,4 +33,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
