69f471cc60f0c5c389e8503f41118d94641099ff
Kabir Maniar <kabir.maniar@maniartech.com>
2021-10-14 11:36:14 +0530

    Added a basic VM that reads and stores objects in its stack

---
+++ vm/vm.go
@@ -0,0 +1,67 @@
+package vm
+
+import (
+	"fmt"
+
+	"github.com/kabironline/monke/code"
+	"github.com/kabironline/monke/compiler"
+	"github.com/kabironline/monke/object"
+)
+
+const StackSize = 2048
+
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+	stack        []object.Object
+	sp           int // Always points to the next value. Top of stack is stack[sp-1]
+}
+
+func New(bytecode *compiler.Bytecode) *VM {
+	return &VM{
+		instructions: bytecode.Instructions,
+		constants:    bytecode.Constants,
+		stack:        make([]object.Object, StackSize),
+		sp:           0,
+	}
+}
+
+func (vm *VM) StackTop() object.Object {
+	//Checking if the stack pointer is at value 0 or the bottom of the stack if yes then returning nil.
+	if vm.sp == 0 {
+		return nil
+	}
+	//If the Stack Pointer is not 0 then pointing the next value at the top [sp-1].
+	return vm.stack[vm.sp-1]
+}
+
+func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+
+		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (vm *VM) push(o object.Object) error {
+	//Checking if the Stack Pointer is greater than the StackSize
+	//If yes then Printing a Stack Overflow error 
+	if vm.sp >= StackSize {
+		return fmt.Errorf("Stack Overflow")
+	}
+
+	vm.stack[vm.sp] = o
+	vm.sp++
+
+	return nil
+}
