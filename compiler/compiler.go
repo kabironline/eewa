f057b55c8aac63c783e7073c616a72301cf06f57
Kabir Maniar <kabir.maniar@maniartech.com>
2021-10-14 12:06:22 +0530

    Added Infix Operator functionality only "+" is supported for now

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,6 +1,8 @@
 package compiler
 
 import (
+	"fmt"
+
 	"github.com/kabironline/monke/ast"
 	"github.com/kabironline/monke/code"
 	"github.com/kabironline/monke/object"
@@ -20,33 +22,35 @@ func New() *Compiler {
 func (c *Compiler) Compile(node ast.Node) error {
 	switch node := node.(type) {
 	case *ast.Program:
-	for _, s := range node.Statements {
-	err := c.Compile(s)
-	if err != nil {
-	return err
-	}
-	}
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
 	case *ast.ExpressionStatement:
-	err := c.Compile(node.Expression)
-	if err != nil {
-	return err
-	}
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
 	case *ast.InfixExpression:
-	err := c.Compile(node.Left)
-	if err != nil {
-	return err
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+		switch node.Operator {
+		case "+":
+			c.emit(code.OpAdd)
+		default:
+			return fmt.Errorf("unknown operator %s", node.Operator)
+		}
 	}
-	err = c.Compile(node.Right)
-	if err != nil {
-	return err
-	}
-	case *ast.IntegerLiteral:
-		integer := &object.Integer{Value: node.Value}
-		c.emit(code.OpConstant, c.addConstant(integer))
-	}
-	return nil
-	}
-	
+}
+
 func (c *Compiler) Bytecode() *Bytecode {
 	return &Bytecode{
 		Instructions: c.instructions,
@@ -68,9 +72,9 @@ func (c *Compiler) emit(op code.Opcode, operands ...int) int {
 	ins := code.Make(op, operands...)
 	pos := c.addInstruction(ins)
 	return pos
-	}
-	func (c *Compiler) addInstruction(ins []byte) int {
+}
+func (c *Compiler) addInstruction(ins []byte) int {
 	posNewInstruction := len(c.instructions)
 	c.instructions = append(c.instructions, ins...)
 	return posNewInstruction
-	}
+}
