b2051c5c5583b622dc67185ea392924f082edca2
Kabir Maniar <kabir.maniar@maniartech.com>
2021-10-19 13:47:52 +0530

    Added Boolean Literals Opcodes and their compilation

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -37,6 +37,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(integer))
+	case *ast.Boolean:
+		if node.Value {
+			c.emit(code.OpTrue)
+		} else {
+			c.emit(code.OpFalse)
+		}
 	case *ast.InfixExpression:
 		err := c.Compile(node.Left)
 		if err != nil {
