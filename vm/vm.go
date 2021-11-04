a9ed308b596f453041afcbfdf286bdf9250855bb
Kabir Maniar <kabir.maniar@maniartech.com>
2021-11-04 12:35:56 +0530

    Added null as a Value to VM

--- vm/vm.go
+++ vm/vm.go
@@ -209,6 +209,8 @@ func (vm *VM) executeBangOperator() error {
 		return vm.push(False)
 	case False:
 		return vm.push(True)
+	case Null:
+		return vm.push(True)
 	default:
 		return vm.push(False)
 	}
@@ -227,6 +229,8 @@ func isTruthy(obj object.Object) bool {
 	switch obj := obj.(type) {
 	case *object.Boolean:
 		return obj.Value
+	case *object.Null:
+		return false
 	default:
 		return true
 	}
