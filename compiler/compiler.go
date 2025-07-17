d2528d0dd0d5ed96fc47cf340c884b96ef463d20
Kabir Maniar <kabir.maniar@maniartech.com>
2025-07-17 08:57:54 +0530

    Add string concatenation support in VM and update binary operation handling

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -105,6 +105,9 @@ func (c *Compiler) Compile(node ast.Node) error {
 		} else {
 			c.emit(code.OpFalse)
 		}
+	case *ast.StringLiteral:
+		str := &object.String{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(str))
 	case *ast.InfixExpression:
 		if node.Operator == "<" {
 			err := c.Compile(node.Right)
